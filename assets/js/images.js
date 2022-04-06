2094bc8fb793f72ec56e89db834678970ce53fae
Matheus Bruno <matheusbrunno35@gmail.com>
2022-04-06 14:56:52 -0300

    second commit

--- assets/js/images.js
+++ assets/js/images.js
@@ -3,5 +3,4 @@ const images = [
     {image: 'assets/images/yasuo.png'},
     {image: 'assets/images/illidan.jpg'},
     {image: 'assets/images/ilidan.jpg'},
-    {image: 'assets/images/naruto.jpg'}
 ]
